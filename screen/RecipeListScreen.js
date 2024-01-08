db02e598ed56d63e292ce20fcb5619a2e87724ef
anitaputri <anitaaramadhani@gmail.com>
2024-01-08 20:04:27 +0700

    menambahkan listscreen dan componen awal

---
+++ screen/RecipeListScreen.js
@@ -0,0 +1,30 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Header from "../components/Header";
+import SearchFilter from "../components/SearchFilter";
+
+const RecipeListScreen = ({ icon, placeholder }) => {
+    return (
+        <SafeAreaView style={{ flex: 1, marginHorizontal: 16 }}>
+            {/* render header */}
+            <Header headerText={"Hi, Everyone"} headerIcon={"bell-o"} />
+
+            {/* Search Filter */}
+            <SearchFilter icon ="search" placeholder={"pilih resep yang kamu inginkan"} />
+
+            {/* Categories filter */}  
+
+            <View>
+                style={{ marginTop: 22 }}
+				<Text style={{ fontSize: 22, fontWeight: "bold" }}>Categories</Text>
+				{/* Categories list */}
+				<CategoriesFilter />
+            </View>
+        </SafeAreaView>
+            
+    );
+};
+
+export default RecipeListScreen;
+
+const style = StyleSheet.create({}); 
